8a160e8b3823535a06f3938f863e6fee7c9f9fa1
Arunkumarreddy <arunkumarreddy127@gmail.com>
2026-08-01 15:56:40 +0000

    Qwen/Qwen2.5-Coder-14B-Instruct

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 COMFY_HOME="/workspace/runpod-slim/ComfyUI" 
 MODEL_DIR="$COMFY_HOME/models/qwen" 
-MODEL_FILE="$MODEL_DIR/Qwen2.5-Coder-14B" 
+MODEL_FILE="$MODEL_DIR/Qwen2.5-Coder-14B-Instruct" 
 
 echo "$MODEL_FILE"
 
@@ -13,7 +13,7 @@ if [ ! -f "$MODEL_FILE" ]; then
     echo "Downloading..."
    
  hf download \
-     Qwen/Qwen2.5-Coder-14B \
+     Qwen/Qwen2.5-Coder-14B-Instruct \
      --local-dir "$MODEL_DIR" 
 
 else 
